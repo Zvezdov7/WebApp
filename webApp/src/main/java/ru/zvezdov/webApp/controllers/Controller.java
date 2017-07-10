b8cc582e9c13ef9754ba530826c91373248ae970
Zvezdov <dszvezdo@mts.ru>
2017-07-10 17:53:11 +0300

    Common controller's annotations example

--- webApp/src/main/java/ru/zvezdov/webApp/controllers/Controller.java
+++ webApp/src/main/java/ru/zvezdov/webApp/controllers/Controller.java
@@ -1,21 +1,64 @@
 package ru.zvezdov.webApp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.zvezdov.webApp.model.Appointment;
+import ru.zvezdov.webApp.model.AppointmentBook;
+import ru.zvezdov.webApp.model.AppointmentForm;
+
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by Dmitry on 10.07.2017.
  */
 @org.springframework.stereotype.Controller
-@RequestMapping("/hello")
+@RequestMapping("/app")
 public class Controller {
-    @RequestMapping(method = RequestMethod.GET)
+    private final AppointmentBook appointmentBook;
+
+    @Autowired
+    public Controller(AppointmentBook appointmentBook) {
+        this.appointmentBook = appointmentBook;
+    }
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello(Model model) {
 //        model.addAttribute("message", "Hello World");
         return "index";
     }
 
+    @GetMapping
+//    @GetMapping(path = "/pets/{petId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Map<String, Appointment> get() {
+        return appointmentBook.getAppointmentsForToday();
+    }
+
+    @RequestMapping(path = "/{day}", method = RequestMethod.GET)
+    public Map<String, Appointment> getForDay(@PathVariable @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) Date day, Model model) {
+        return appointmentBook.getAppointmentsForDay(day);
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public AppointmentForm getNewForm() {
+        return new AppointmentForm();
+    }
+
+    @PostMapping
+    public String add(/*@Valid*/ AppointmentForm appointment, BindingResult result) {
+        if (result.hasErrors()) {
+            return "appointments/new";
+        }
+        appointmentBook.addAppointment(appointment);
+        return "redirect:/appointments";
+    }
+
+    @PostMapping(path = "/pets", consumes = "application/json")
+    public void addPet(@RequestBody Appointment pet, Model model) {
+        // implementation omitted
+    }
 }
---
+++ webApp/src/main/java/ru/zvezdov/webApp/model/AppointmentForm.java
@@ -0,0 +1,7 @@
+package ru.zvezdov.webApp.model;
+
+/**
+ * Created by Dmitry on 10.07.2017.
+ */
+public class AppointmentForm {
+}
