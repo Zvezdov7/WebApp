9531eeb044914a1808fc595b8984a2989626f721
Zvezdov <dszvezdo@mts.ru>
2017-07-10 16:54:17 +0300

    First working version. Return simple string as result on request.

--- webApp/src/main/java/ru/zvezdov/webApp/controllers/Controller.java
+++ webApp/src/main/java/ru/zvezdov/webApp/controllers/Controller.java
@@ -2,16 +2,20 @@ package ru.zvezdov.webApp.controllers;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Dmitry on 10.07.2017.
  */
 @org.springframework.stereotype.Controller
+@RequestMapping("/hello")
 public class Controller {
-    @RequestMapping("/hello")
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
     public String hello(Model model) {
-        model.addAttribute("message", "Hello World");
-        return "Hello";
+//        model.addAttribute("message", "Hello World");
+        return "index";
     }
 
 }
